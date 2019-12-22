11fa9bdf66f4a9be14fa5fbc4afd4fc8952431b9
Justin Phillips <justinphil02@yahoo.com>
2019-12-22 00:53:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    
+    drop_table :shows 
     create_table :shows do |col|
       col.string :name 
       col.string :network
